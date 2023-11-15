2e33b0065207273378140b6de5d93b5c2674ffff
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2023-11-15 19:41:16 +0300

    modified task.js

--- task.js
+++ task.js
@@ -2,28 +2,25 @@ const taskText = document.getElementById('task__input')
 const btnAddTask = document.getElementById('tasks__add')
 const tasksList = document.getElementById('tasks__list')
 
-taskText.addEventListener('keydown', addTask)
 btnAddTask.addEventListener('click', addTask)
-document.addEventListener('click', delTask)
+tasksList.addEventListener('click', delTask)
 
 function addTask(event) {
-    if ((event.key == 'Enter' || event.type == 'click') && taskText.value.length != 0) {
-        tasksList.innerHTML +=
+    if (taskText.value.trim() !== '') {
+        tasksList.insertAdjacentHTML('afterBegin',
          `<div class="task">
             <div class="task__title">
                 ${taskText.value}
             </div>
             <a href="#" class="task__remove">&times;</a>
           </div>`
+        )        
         
-        taskText.value = null
-        
-        if (event.type == 'click') {
-            taskText.focus()
-        }
-
-        event.preventDefault()
+        taskText.value = ''
     }
+    
+    taskText.focus()
+    event.preventDefault()
 }
 
 function delTask (event) {
